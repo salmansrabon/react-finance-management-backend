db956d35534dd984ebdd0290b9f6f2d144ba66e1
salmansrabon <salmansrabon@gmail.com>
2024-10-02 01:05:33 +0600

    change remote repository name

--- controllers/userController.js
+++ controllers/userController.js
@@ -45,7 +45,7 @@ const updateUser = async (req, res) => {
       return res.status(404).json({ message: 'User not found' });
     }
 
-    res.status(200).json(updatedUser); // Return the updated user data
+    res.status(200).json(updatedUser);
   } catch (err) {
     res.status(500).json({ message: 'Server error', error: err.message });
   }
